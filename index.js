780952691ac6a4bdee8497af8226a341a47504c8
Lucy Wall <lhwall@gmail.com>
2018-01-17 20:35:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
+function sayHitoGrandma(string){
+  if (string === "I love you, Grandma")
+  {return "I love you too"}
+  else if (string === string.toLowerCase)
+  {return WORDS}
+  else if (string === string.toUpperCase)
+  {return "YES INDEED!"}
+  else return "You ungrateful child"
+}
